76c8da049374335c3a7abff3571756fd0466a059
Hunter Dale <guyyatsu@gmail.com>
2021-10-25 04:37:59 +0000

    Formatting, Commenting.

--- flaskball/flask_ball.py
+++ flaskball/flask_ball.py
@@ -3,19 +3,39 @@ from os import read
 from flask import Flask, render_template, request, flash
 from random import *
 
-readings = ["No", "Definitely not", "Yes!", "Nope!", "Ask another time.", "Yessir"]
 
-def reader(): return choice(readings)
-EightBall = Flask(__name__); EightBall.secret_key = "ramen"
+readings = [# More answers; Better formatting.
+  "No.",
+  "Definitely not!",
+  "Yes!",
+  "Nope!",
+  "Ask another time.",
+  "Yessir!",
+  "Outlook Good.",
+  "Maybe?",
+  "Can't say...",
+  "Not certain."
+]
+
+def reader(): return choice(readings)# One-Liner; make it so.
+EightBall = Flask(__name__); EightBall.secret_key = "ramen"# Related;
 
 @EightBalll.route("/", methods=["POST", "GET"])
 def index():
+
     if request.method == 'POST':
         if request.form.get('submitbutton') == 'Shake The EightBall':
-            flash(reader())
-            return render_template('index.html')
+            flash(reader());
+            return render_template(
+                'index.html'
+            )
+
     elif request.method == 'GET':
         return render_template('index.html')
 
-if __name__ == '__main__':
-    EightBall.run(host="localhost", port=8080, debug=True)
+if __name__ == '__main__': EightBall.run(
+    # List-Notation for ease of config.
+    host="localhost",
+    port=8080,
+    debug=True
+)
